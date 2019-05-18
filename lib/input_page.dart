890d3a3b7b17264c2f77048f6c69df7dee21f8d1
yuweh <francisb.itcareers@gmail.com>
2019-05-19 07:22:34 +0800

    customized extracted widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,12 +9,58 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReusableCard(
+                  colour: Color(0XFF1D1E33),
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  colour: Color(0XFF1D1E33),
+                )),
+              ],
+            )),
+            Expanded(
+                child: ReusableCard(
+              colour: Color(0XFF1D1E33),
+            )),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReusableCard(
+                  colour: Color(0XFF1D1E33),
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  colour: Color(0XFF1D1E33),
+                )),
+              ],
+            )),
+          ],
+        )); //Scaffold
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ), //Decoration
     );
   }
 }
